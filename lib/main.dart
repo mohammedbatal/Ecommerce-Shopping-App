82745ab8e451f19afbe2895c7225a25486064de9
mohammed batal <s.cmohammedbatal@gmail.com>
2024-12-26 12:16:17 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:e_commerce/theme/theme.dart';
+import 'package:e_commerce/view/screen/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness: Brightness.dark,
+    statusBarBrightness: Brightness.light,
+  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override 
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ShopEase',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.LightTheme, 
+      home: SplashScreen(),
+    );
+  }
+}
