1a3fdef389181247d5bf8df4dc5d9cf3c8c64e2b
mohammed batal <s.cmohammedbatal@gmail.com>
2024-12-27 12:00:58 +0300

    SplashScreen

---
+++ lib/view/screen/onbording_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OnbordingScreen extends StatelessWidget {
+  const OnbordingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
